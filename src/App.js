d2808d2fac2da5684ce1e1d1a2ef8998d83d99e9
Roy CH Lee <choonghyun.roy.lee@gmail.com>
2020-02-13 13:14:25 +0900

    lesson 5

--- src/App.js
+++ src/App.js
@@ -1,19 +1,44 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Customer from './components/Customer'
 import './App.css';
 
+const customers = [
+  {
+  'id':1,
+  'image':'https://placeimg.com/64/64/any',
+  'name':'홍길동',
+  'birthday':'801117',
+  'gender':'남자',
+  'job':'대학생'
+  },
+  {
+    'id':1,
+    'image':'https://placeimg.com/64/64/any',
+    'name':'홍길동',
+    'birthday':'801117',
+    'gender':'남자',
+    'job':'대학생'
+  }
+]
 //this is the first test.
 class App extends Component {
   render(){
     return (
-        <div className="gray-background">
-          <img src={logo} alt="logo" />
-          <h2> Let's develop management system! </h2>
-        </div>
+      <div>
+      {customers.map(c=>{
+       return <Customer
+               key={c.id}
+               id={c.id}
+               image={c.image}
+               name={c.name}
+               birthday={c.birthday}
+               ender={c.gender}
+               job={c.job}
+      />
+      })}
+      </div>
     );
   }
 }
-
-jtes
 
 export default App;
